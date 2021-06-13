942822476ff457fe8f77192febbad53bf16a1219
Vaidhyaprakash <ivaidhyaprakash@gmail.com>
2021-06-13 23:58:56 +0530

    Created User and Post table and added relationship btw them, done get,post put delete operations

--- models/user.js
+++ models/user.js
@@ -9,22 +9,43 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate({Post}) {
       // define association here
+      this.hasMany(Post,{foreignKey:'userId', as: 'posts'});
+    }
+    toJSON(){
+      return {...this.get(),id:undefined}
     }
   };
   User.init({
+    uuid:{
+      type:DataTypes.UUID,
+      defaultValue:DataTypes.UUIDV4,
+    },
     name: {
       type:DataTypes.STRING,
       allowNull:false,
+      validate:{
+        notNull:{msg:'Name for User is must'},
+        notEmpty:{msg:'Name cannot be Empty'}
+      }
     },
     email: {
       type:DataTypes.STRING,
       allowNull:false,
+      validate:{
+        notNull:{msg:'email for User is must'},
+        notEmpty:{msg:'email cannot be Empty'},
+        isEmail: {msg : 'email must be in a@b.com format'}
+      }
     },
     role: {
       type:DataTypes.STRING,
       allowNull:false,
+      validate:{
+        notNull:{msg:'Role for User is must'},
+        notEmpty:{msg:'Role cannot be Empty'}
+      }
     }
   }, {
     sequelize,
--- server.js
+++ server.js
@@ -2,13 +2,9 @@ const Hapi = require('@hapi/hapi');
 const bodyParser = require("body-parser");
 //const pool = require("./db");
 const { Sequelize } = require('sequelize');
-const {sequelize} = require("./models");
+const {sequelize,User,Post} = require("./models");
+const user = require('./models/user');
 
-
-async function main(){
-  await sequelize.sync({force: true});
-}
-main();
 
 //..........................Handling Routes.....................
 const init = async () => {
@@ -29,15 +25,131 @@ const init = async () => {
 
     server.route({
         method: 'POST',
-        path: '/Person',
-        handler: function(request, h) {
-          const name = request.payload.name;
+        path: '/users',
+        handler: async function users(request, h) {
 
+            const {name,email,role}=request.payload;
+            try{
+                const user = await User.create({name,email,role});
+                return h.response(user);
+            }catch(err){
+                console.log(err);
+                return "Error";
+            }
         }
     });
 
+    server.route({
+        method: 'GET',
+        path: '/users',
+        handler: async function(request, h) {
+            try{
+                const users= await User.findAll({include : ['posts']});
+                return h.response(users);
+            }catch(err){
+                console.log(err);
+                return "Error";
+            }
+            
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/users/{name}',
+        handler: async function(request, h) {
+            const name = request.params.name;
+            try{
+                const user= await User.findOne({
+                    where: { name },
+                    include:['posts'],
+                });
+                return h.response(user);
+            }catch(err){
+                console.log(err);
+                return "Error";
+            }
+            
+        }
+    });
+
+    server.route({
+        method: 'POST',
+        path: '/posts',
+        handler: async function posts(request, h) {
+
+            const {body,userUuid}=request.payload;
+            try{
+                const user = await User.findOne({where:{uuid:userUuid}});
+                const post = await Post.create({body,userId:user.id});
+                return h.response(post);
+            }catch(err){
+                console.log(err);
+                return "Error";
+            }
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/posts',
+        handler: async function posts(request, h) {
+
+            try{
+                const posts = await Post.findAll({include:['user']});
+                return h.response(posts);
+            }catch(err){
+                console.log(err);
+                return "Error";
+            }
+        }
+    });
+    server.route({
+        method: 'DELETE',
+        path: '/users/{name}',
+        handler: async function(request, h) {
+            const name = request.params.name;
+            try{
+                const user= await User.findOne({
+                    where: { name }
+                });
+                await user.destroy();
+                return "User Deleted";
+            }catch(err){
+                console.log(err);
+                return "Error";
+            }
+            
+        }
+    });
+    server.route({
+        method: 'PUT',
+        path: '/users/{name}',
+        handler: async function(request, h) {
+            const nameSearch = request.params.name;
+            const {name,email,role}=request.payload;
+            try{
+                const user= await User.findOne({
+                    where: { name:nameSearch }
+                });
+                user.name=name;
+                user.email=email;
+                user.role=role;
+                user.save();
+                return h.response(user);
+            }catch(err){
+                console.log(err);
+                return "Error";
+            }
+            
+        }
+    });
+
+
     await server.start();
     console.log('Server running on', server.info.uri);
+    await sequelize.authenticate();
+    console.log("Database Connected...!");
 };
 
 
